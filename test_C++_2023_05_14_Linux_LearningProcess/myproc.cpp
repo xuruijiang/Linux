d0fb7a9fd4f6f9ae55123213a41e87b2cb66f541
2451603830@qq.com <2451603830@qq.com>
2023-05-16 13:51:43 +0800

    学习进程

--- test_C++_2023_05_14_Linux_LearningProcess/myproc.cpp
+++ test_C++_2023_05_14_Linux_LearningProcess/myproc.cpp
@@ -3,9 +3,35 @@
 using namespace std;
 int main()
 {
-    fork();
+    pid_t id = fork(); //int
 
-    cout << "hello proc: " << getpid() << "hello parent: " << getppid() << endl;
-    sleep(1);
+    if(id == 0)
+    {
+      
+        cout << "I an child, PID: " << getpid() << " PPID: " << getppid() << endl;
+        while(true)
+        {
+            cout << "I an child " << endl;
+            sleep(2);
+        }
+    }
+    else if(id > 0)
+    {
+        
+        cout << "I an father, PID: " << getpid() << " PPID: " << getppid() << endl;
+        int i = 10;
+        while(i > 1)
+        {
+            cout << "I an father" << endl;
+            sleep(1);
+            i--;
+        }
+        //exit(1);
+    }
+    else{
+      ;
+    }
+//    sleep(100);
+//
     return 0;
 }
